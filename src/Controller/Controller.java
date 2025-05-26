d8a3479de8f3a073b15ec69b85e3f4042584b7a2
Paul De La Toba <pauldelatoba9@gmail.com>
2025-05-26 09:47:06 -0700

    Prueba 3

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -36,7 +36,7 @@ public class Controller implements ActionListener {
 
     public boolean validateLogin(){
         /*Aqui agregar validaciones del login*/
-        return true;
+        return false;
     }
 
     public void openMainView(){
