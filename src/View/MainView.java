a60ba0e3a9a6efe153186f3697ea623fb1755edf
Paul de la Toba <pauldt9>
2025-05-25 18:18:37 -0700

    avance 1.9

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -12,37 +12,60 @@ public class MainView extends JPanel {
         setBackground(new Color(0x4A66CA));
         setLayout(new BorderLayout());
 
+        /*Panel superior*/
         JPanel topPanel = new JPanel();
         topPanel.setBackground(new Color(0x4A66CA));
-        topPanel.setLayout(new FlowLayout());
-        
+        topPanel.setLayout(new BorderLayout());
+        topPanel.setOpaque(false);
+        topPanel.setPreferredSize(new Dimension(getWidth(), 130));
+        add(topPanel, BorderLayout.NORTH);
 
         try {
             logo = ImageIO.read(new File("src/img/logo.png"));
-            logo = logo.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            logo = logo.getScaledInstance(120, 120, Image.SCALE_SMOOTH);
         } catch (IOException e){
             System.out.println("Hubo un error al cargar la imagen: " + e.getMessage());
         }
 
+        JLabel title = createJLabel("INICIO", 40, 1);
+        title.setBorder(BorderFactory.createEmptyBorder(5, 150, 5, 5));
+        topPanel.add(title, BorderLayout.WEST);
+
+        /*Panel del menu*/
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.WHITE);
+        centerPanel.setLayout(new GridBagLayout());
+        centerPanel.setPreferredSize(new Dimension(getWidth(), getHeight()));
+        add(centerPanel, BorderLayout.CENTER);
+
+
+
     }
 
-    public JLabel createJLabel(String title, int fontSize){
+    public JLabel createJLabel(String title, int fontSize, int color){
         JLabel label = new JLabel(title);
         label.setFont(new Font("Helvetica Neue", Font.BOLD, fontSize));
-        label.setForeground(new Color(30, 30, 30));
+
+        if (color == 1){
+            label.setForeground(Color.WHITE);
+        } else {
+            label.setForeground(new Color(30, 30, 30));
+        }
+
         return label;
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        g.drawImage(logo, 20, 5, this);
 
         Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Composite original = g2.getComposite(); //obtener la composicion original
 
-        /*Rectangulo superior*/
+        g2.setColor(new Color(0, 0, 0, 50));
+        g2.fillRoundRect(1078, 10, 300, 110, 15, 15);
 
-        
-        
+        g2.setComposite(original); //restablecer
     }
 }
