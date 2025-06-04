698322268ea0a46ee2321237144e046411cec344
Paul De La Toba <pauldelatoba9@gmail.com>
2025-06-04 02:38:05 -0700

    otro avance

--- src/View/Employee/SelectSeats.java
+++ src/View/Employee/SelectSeats.java
@@ -1,9 +1,16 @@
 package View.Employee;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SelectSeats extends JPanel {
+
     public SelectSeats(){
+        setLayout(new BorderLayout());
+        setOpaque(false);
+
 
     }
+
+
 }
